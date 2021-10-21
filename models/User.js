8771d1f621d2c9d857d0a50a2728525959ad1e0c
Man E <mannyportillo77@gmail.com>
2021-10-21 16:19:21 -0600

    added schema to User model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,34 @@
 const { Schema, model } = require("mongoose");
 
+const UserSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: "Please provide a username.",
+        trim: true,
+      },
+      email: {
+        type: String,
+        unique: true,
+        required: "Please enter your email.",
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Thought",
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ]
+    }
+);
+
+const User = model("User", UserSchema);
+
 module.exports = User;
