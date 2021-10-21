3a2d591f9e92423619ed67592cfd4cde5751a2e4
Man E <mannyportillo77@gmail.com>
2021-10-21 16:11:51 -0600

    created Thought Schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,26 @@
 const { Schema, model, Types } = require("mongoose");
 
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        // reactions:
+    }
+);
+
+const Thought = model("Thought", ThoughtSchema);
+
 module.exports = Thought;
